d4fa9ce6d221f602ec156ffa2e6773617f5c8c3c
kooya3 <elyeestatua@gmail.com>
2023-02-10 02:52:27 +0300

    added a few images and edited the landing page

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -16,8 +16,8 @@ const HeroBanner = () => (
     <Stack>
       <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
     </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
-      Exercise
+    <Typography fontWeight={570} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+     Sparked
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
