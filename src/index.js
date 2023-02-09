814fe6430c315ad930c2636c40b34cec2d65d9c1
kooya3 <elyeestatua@gmail.com>
2023-02-09 09:54:09 +0300

    added required dependencies and style

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import REact from 'react';
+import  ReactDOM from 'react';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <App />
+)
