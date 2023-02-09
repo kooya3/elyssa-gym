946343478eb8eb5c8a27319e26394622442fbc24
kooya3 <elyeestatua@gmail.com>
2023-02-09 10:05:52 +0300

    new files and folders created with few edits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} /> 
+      </Routes>
+    </Box>
   )
 }
 
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExerciseDetail = () => {
+  return (
+    <div>ExerciseDetail</div>
+  )
+}
+
+export default ExerciseDetail
