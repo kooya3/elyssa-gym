d4a0c2de1325d51417dce2e4c337e948bb03485f
kooya3 <elyeestatua@gmail.com>
2023-02-09 10:10:13 +0300

    made necessary imports to app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import './App.css'
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
