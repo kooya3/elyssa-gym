a20d6eff51219e7e9c39205815a6f75f637c90a6
kooya3 <elyeestatua@gmail.com>
2023-02-09 23:31:40 +0300

    new file changes made to each component

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Box, Stack, Typography, Button } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
+import HeroBannerImage from '../assets/images/banner.png';
 
-const HeroBanner = () => {
-  return (
-    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
@@ -21,7 +21,6 @@ const HeroBanner = () => {
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
-  )
-}
+);
 
-export default HeroBanner
+export default HeroBanner;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import  ReactDOM from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
@@ -7,7 +7,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
+  </React.StrictMode>,
 );
+
