ec3244c4436fcf4bfe315feb5f6d719e3435ddb5
kooya3 <elyeestatua@gmail.com>
2023-02-10 03:25:10 +0300

    made new objects and perfected the search-logic

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
  
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState('');
+  const [bodyParts, setBodyParts] = useState([]);
+
    return (
      <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography fontWeight={700} sx={{fontSize: { lg: '45px', xs: '30px'}}} mb="46px" textAlign="center">
