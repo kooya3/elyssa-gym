3f68a6dd6199afc6992d179fe2403046e5f4e88f
kooya3 <elyeestatua@gmail.com>
2023-02-12 02:38:23 +0300

    created new components files for detail, exercisevideo & similarexs

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -8,43 +8,61 @@ import Loader from './Loader';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const exercisesPerPage = 12;
+  const [exercisesPerPage] = useState(9);
 
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
+  // Pagination
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
   const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
-  const paginate = (e, value) => {
+  const paginate = (event, value) => {
     setCurrentPage(value);
 
-    window.scrollTo({ top: 1800, behaviour: 'smooth'})
-  }
-  
-  return (
-    <Box id="exercises" sx={{mt: { lg: '110px'}}} mt="50px" p="20px">
-    <Typography variant="h3" mb="46px">
-      Showing Results
-    </Typography>
-    <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
-      {exercises.map((exercise, index) => (
-        <ExerciseCard key={index} exercise={exercise} />       
-      ))}
-    </Stack>
-    <Stack mt="100px" alignItems="center">
-      {exercises.length > 12 && (
-        <Pagination color="standard"
-        shape="rounded"
-        defaultPage={1}
-        count={math.ceil(exercises.length / exercisesPerPage)}
-        /* exercise number is 12 per page */
-        page={currentPage}
-        onChange={paginate}
-        size="large" />
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+  };
 
-      )}
-    </Stack>
+  if (!currentExercises.length) return <Loader />;
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
+      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
     </Box>
   );
-}
+};
 
-export default Exercises
+export default Exercises;
+
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
+
 
 const ExerciseDetail = () => {
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
