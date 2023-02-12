cf35ff0aeedeb9207b0cb61cdb34944f09e2f08c
kooya3 <elyeestatua@gmail.com>
2023-02-12 04:17:19 +0300

    split the page into similar :{ target muscle & equipment exercise}

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -15,7 +15,10 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
     <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
       Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> exercises
     </Typography>
-
+    <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+      {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
+    </Stack>
+  </Box>
 );
 
 export default SimilarExercises
