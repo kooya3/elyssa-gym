4a4ba34e5c3a4a1bddc722802185b7f06d9379e5
kooya3 <elyeestatua@gmail.com>
2023-02-12 03:21:06 +0300

    made a few edits to create the exercise UI/UX

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { Typography, Stack, Button } from '@mui/material';
+
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
