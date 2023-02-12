90ff87bf33b0f79954f0c0da81be68334fd20eaf
kooya3 <elyeestatua@gmail.com>
2023-02-12 04:22:00 +0300

    created a stack component with infinitespin

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,8 +4,8 @@ import { InfinitySpin } from 'react-loader-spinner';
 
 const Loader = () => (
   <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
-    <div>Loader</div>
-  )
-}
+    <InfinitySpin color="grey" />
+    </Stack>
+);
 
 export default Loader
