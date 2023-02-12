4ca0090e58c5b8d7a631241aad789d862e9f0fed
kooya3 <elyeestatua@gmail.com>
2023-02-12 04:20:42 +0300

    started on the loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { Stack } from '@mui/material';
+import { InfinitySpin } from 'react-loader-spinner';
 
-const Loader = () => {
-  return (
+const Loader = () => (
+  <Stack direction="row" justifyContent="center" alignItems="center" width="100%">
     <div>Loader</div>
   )
 }
