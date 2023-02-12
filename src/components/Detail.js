e1f8dfa497b0dfb279abe43a9e3e215d18b991ec
kooya3 <elyeestatua@gmail.com>
2023-02-12 03:07:14 +0300

    necessary assets added and new objects made

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Typography, Stack, Button } from '@mui/material';
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
 
-const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
-}
+const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
 
 export default Detail
