d07c5fe5d0b6130c7b954ac8f1cac73e7de09139
kooya3 <elyeestatua@gmail.com>
2023-02-12 04:11:43 +0300

    started on similarExercises.js file

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Box, Stack } from '@mui/material';
+import HorizontalScrollbar from './HorizontalScrollbar';
+import Loader from './Loader';
 
-const SimilarExercises = () => {
-  return (
-    <div>SimilarExercises</div>
-  )
-}
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
+
+    <Box sx={{
+      mt: { lg: '100px', xs: '0px' }
+    }});
 
 export default SimilarExercises
